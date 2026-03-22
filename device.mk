d24a2515e2011227a6bfef290dd2b8a73fa13e19
Balarama Sahu <sahubalarama739@gmail.com>
2026-03-22 22:04:19 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,13 +10,14 @@ LOCAL_PATH := device/xiaomi/beryl
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+      android.hardware.boot@1.2-impl
+      android.hardware.boot@1.2-impl.recovery
+      android.hardware.boot@1.2-service   
 
 PRODUCT_PACKAGES += \
     bootctrl.mt6855
@@ -33,3 +34,13 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# ADD after existing PRODUCT_PACKAGES:
+PRODUCT_PACKAGES += \
+    com.android.hardware.boot \
+    android.hardware.boot-service.default_recovery
+
+
+# ADD:
+PRODUCT_PACKAGES += \
+    fastbootd
